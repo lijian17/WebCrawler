1527a95b2a97e9461f9c4ca9a3994077b8fa06d9
深情小建 <374452668@qq.com>
2019-07-04 11:25:52 +0800

    hello world运行

---
+++ app.js
@@ -0,0 +1,17 @@
+// 入口文件
+var express = require('express');
+var app = express();
+
+// 主页输出 "Hello World"
+app.get('/', function(req, res){
+	res.send('Hello World');
+})
+
+var server = app.listen(3000, function(){
+	var host = server.address().address;
+	var port = server.address().port;
+	
+	console.log('应用实例，访问地址为 http://%s:%s', host, port);
+})
+
+
