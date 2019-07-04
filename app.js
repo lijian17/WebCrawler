345611a2e8dea50e5748128ab01e44b1eecc57ba
深情小建 <374452668@qq.com>
2019-07-04 12:16:05 +0800

    添加爬虫

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
 // 入口文件
 var express = require('express');
+var superagent = require('superagent');
+var cheerio = require('cheerio');
+
 var app = express();
 
 // 静态文件路径设置
 app.use('/static', express.static(__dirname + '/static'));
 
-// 主页输出 "Hello World"
-app.get('/', function(req, res){
-	res.send('Hello World');
-})
+app.get('/', function(req, res, next) {
+	// 用 superagent 去抓取 https://cnodejs.org/ 的内容
+	superagent.get('https://cnodejs.org/')
+		.end(function(err, sres) {
+			// 常规的错误处理
+			if(err) {
+				return next(err);
+			}
+			// sres.text 里面存储着网页的 html 内容，将它传给 cheerio.load 之后
+			// 就可以得到一个实现了 jquery 接口的变量，我们习惯性地将它命名为 `$`
+			// 剩下就都是 jquery 的内容了
+			var $ = cheerio.load(sres.text);
+			var items = [];
+			$('#topic_list .topic_title').each(function(idx, element) {
+				var $element = $(element);
+				items.push({
+					title: $element.attr('title'),
+					href: $element.attr('href')
+				});
+			});
+			res.send(items);
+		});
+});
 
-var server = app.listen(3000, function(){
+var server = app.listen(3000, function() {
 	var host = server.address().address;
 	var port = server.address().port;
-	
+
 	console.log('应用实例，访问地址为 http://%s:%s', host, port);
 })
-
-
